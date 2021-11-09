fed949758cbcdbeccd4868d4aa0b9447819ff57e
Jibran Kalia <jibran.kalia@gmail.com>
2021-11-08 21:26:31 -0600

    Annotate Property model

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: properties
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  address    :string
+#  price      :decimal(, )
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Property < ApplicationRecord
 end
